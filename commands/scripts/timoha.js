f565bb0d58b99d495402370501f41a7771c85eb3
VadKoles <120252264+VadKoles@users.noreply.github.com>
2023-05-17 14:02:00 +1000

    Add files via upload

---
+++ commands/scripts/timoha.js
@@ -0,0 +1,55 @@
+let doing1 = true
+n1_div.hidden = true
+n1_img.hidden = true
+function n1(){
+    if(doing1 == true){
+        n1_div.hidden = false
+        doing1 = false
+    } else {
+        n1_div.hidden = true
+        doing1 = true
+    }
+}
+function n1_go(){
+    if(n1_inp.value == "137"){
+        n1_img.hidden = false
+    }
+}
+
+
+let doing2 = true
+n2_div.hidden = true
+n2_text.hidden = true
+function n2(){
+    if(doing2 == true){
+        n2_div.hidden = false
+        doing2 = false
+    } else {
+        n2_div.hidden = true
+        doing2 = true
+    }
+}
+function n2_go(){
+    if(n2_inp.value == "ZBS"){
+        n2_text.hidden = false
+    }
+}
+
+
+let doing3 = true
+n3_div.hidden = true
+n3_text.hidden = true
+function n3(){
+    if(doing3 == true){
+        n3_div.hidden = false
+        doing3 = false
+    } else {
+        n3_div.hidden = true
+        doing3 = true
+    }
+}
+function n3_go(){
+    if(n3_inp.value == "152"){
+        n3_text.hidden = false
+    }
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+let passA = "8719"
+let passT = "5492"
+let passD = "2601"
+function toA(){
+    if(password.value == passA){
+        window.location.href = 'commands/andrey.html'
+    }
+}
+function toT(){
+    if(password.value == passT){
+        window.location.href = 'commands/timoha.html'
+    }
+}
+function toD(){
+    if(password.value == passD){
+        window.location.href = 'commands/dimov.html'
+    }
+}
